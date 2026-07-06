71848427e8be447d0290497460170094336b8089
cosimobaldi-unifi <cosimo.baldi1@edu.unifi.it>
2026-07-07 01:36:08 +0200

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g=*kci!j*723c+^6&2ukm$@cy(d$hiv8&mz-n2ykw=6n-ve0$-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
